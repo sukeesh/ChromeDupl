b17e9f8782928d79a45d2e3b107c9b2de9768354
Sukeesh <vsukeeshbabu@gmail.com>
2017-09-02 00:03:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     var x =(tab.url);
     chrome.tabs.create({url:x});
 });
-
-var x = 4;
-var y = 5;
-var ss = document.getElementById("hi')
-
